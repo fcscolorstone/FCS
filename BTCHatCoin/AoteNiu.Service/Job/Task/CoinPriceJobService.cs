8470139001fe661f54fc74d19ec1265ef8136ac7
fcscolorstone <myfcs888@gmail.com>
2020-02-28 09:44:15 +0800

    change to new platform price

--- BTCHatCoin/AoteNiu.Service/Job/Task/CoinPriceJobService.cs
+++ BTCHatCoin/AoteNiu.Service/Job/Task/CoinPriceJobService.cs
@@ -54,9 +54,14 @@ namespace AoteNiu.Service
             {
                 if (_PriceCNY > 0)
                 {
-                    FlushBinancePrice("BTC", "BTCUSDT", "0");
+                    //FlushBinancePrice("BTC", "BTCUSDT", "0");
+                    //Thread.Sleep(2000);
+                    //FlushBinancePrice("ETH", "ETHUSDT", "0x0000000000000000000000000000000000000000");
+                    //Thread.Sleep(2000);
+
+                    FlushHuobiPrice("btc", "btcusdt", "0");
                     Thread.Sleep(2000);
-                    FlushBinancePrice("ETH", "ETHUSDT", "0x0000000000000000000000000000000000000000");
+                    FlushHuobiPrice("eth", "ethusdt", "0x0000000000000000000000000000000000000000");
                     Thread.Sleep(2000);
                 }
             }
